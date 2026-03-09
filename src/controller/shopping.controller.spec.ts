d5a6287482a5b4cedcb0c6fe30203145ce90d49d
Romain Cochonnat <romain.cochonnat@lgce.fr>
2026-03-09 16:18:46 +0100

    refactor: add unit tests for ShoppingController

--- src/controller/shopping.controller.spec.ts
+++ src/controller/shopping.controller.spec.ts
@@ -17,4 +17,56 @@ describe('ShoppingController', () => {
       controller.getPrice({ items: [], type: 'STANDARD_CUSTOMER' }),
     ).not.toThrow();
   });
+
+  it('should return 180 for standard customer with 1 TSHIRT, 1 DRESS, 1 JACKET ', () => {
+    expect(
+      controller.getPrice({
+        items: [
+          { type: 'TSHIRT', nb: 1 },
+          { type: 'DRESS', nb: 1 },
+          { type: 'JACKET', nb: 1 },
+        ],
+        type: 'STANDARD_CUSTOMER',
+      }),
+    ).toBe('180');
+  });
+
+  it('should return 90 for platinum customer with 1 TSHIRT, 1 DRESS, 1 JACKET ', () => {
+    expect(
+      controller.getPrice({
+        items: [
+          { type: 'TSHIRT', nb: 1 },
+          { type: 'DRESS', nb: 1 },
+          { type: 'JACKET', nb: 1 },
+        ],
+        type: 'PLATINUM_CUSTOMER',
+      }),
+    ).toBe('90');
+  });
+
+  it('should throw if customer type unknown', () => {
+    expect(() =>
+      controller.getPrice({
+        items: [
+          { type: 'TSHIRT', nb: 1 },
+          { type: 'DRESS', nb: 1 },
+          { type: 'JACKET', nb: 1 },
+        ],
+        type: 'XXXX_CUSTOMER',
+      }),
+    ).toThrow();
+  });
+
+  it('should throw if basket price > 800 for PREMIUM CUSTOMER', () => {
+    expect(() =>
+      controller.getPrice({
+        items: [
+          { type: 'TSHIRT', nb: 0 },
+          { type: 'DRESS', nb: 0 },
+          { type: 'JACKET', nb: 9 },
+        ],
+        type: 'PREMIUM_CUSTOMER',
+      }),
+    ).toThrow();
+  });
 });
