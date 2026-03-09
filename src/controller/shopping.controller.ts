98af3132ff5ff2877f5ff6243af13abc35388c49
Romain Cochonnat <romain.cochonnat@lgce.fr>
2026-03-09 16:36:15 +0100

    refactor: create getCustomerDiscount + rename some variables

--- src/controller/shopping.controller.ts
+++ src/controller/shopping.controller.ts
@@ -17,7 +17,7 @@ class Item {
   }
 }
 
-class ShoppingBody {
+class Basket {
   items: Item[] | null;
   type: string;
 
@@ -27,30 +27,34 @@ class ShoppingBody {
   }
 }
 
+export const CUSTOMER_DISCOUNT: Record<string, number> = {
+  STANDARD_CUSTOMER: 1,
+  PREMIUM_CUSTOMER: 0.9,
+  PLATINUM_CUSTOMER: 0.5,
+};
+
 @Controller('shopping')
 export class ShoppingController {
   private logger = new Logger(ShoppingController.name);
 
+  public getCustomerDiscount(basket: Basket): number {
+    const customerType = basket.type;
+    if (!(customerType in CUSTOMER_DISCOUNT)) {
+      throw new HttpException('Bad request', HttpStatus.BAD_REQUEST);
+    }
+
+    return CUSTOMER_DISCOUNT[customerType];
+  }
+
   @Post()
-  getPrice(@Body() b: ShoppingBody): string {
-    let p = 0;
-    let d: number;
+  getPrice(@Body() basket: Basket): string {
+    let price = 0;
+    const discount: number = this.getCustomerDiscount(basket);
 
     const date = new Date();
     const cal = new Date(
       date.toLocaleString('en-US', { timeZone: 'Europe/Paris' }),
     );
-
-    // Compute discount for customer
-    if (b.type === 'STANDARD_CUSTOMER') {
-      d = 1;
-    } else if (b.type === 'PREMIUM_CUSTOMER') {
-      d = 0.9;
-    } else if (b.type === 'PLATINUM_CUSTOMER') {
-      d = 0.5;
-    } else {
-      throw new HttpException('Bad request', HttpStatus.BAD_REQUEST);
-    }
 
     // Compute total amount depending on the types and quantity of product and
     // if we are in winter or summer discounts periods
@@ -58,38 +62,38 @@ export class ShoppingController {
       !(cal.getDate() < 15 && cal.getDate() > 5 && cal.getMonth() === 5) &&
       !(cal.getDate() < 15 && cal.getDate() > 5 && cal.getMonth() === 0)
     ) {
-      if (b.items === null) {
+      if (basket.items === null) {
         return '0';
       }
 
-      for (let i = 0; i < b.items.length; i++) {
-        const it = b.items[i];
+      for (let i = 0; i < basket.items.length; i++) {
+        const it = basket.items[i];
 
         if (it.type === 'TSHIRT') {
-          p += 30 * it.nb * d;
+          price += 30 * it.nb * discount;
         } else if (it.type === 'DRESS') {
-          p += 50 * it.nb * d;
+          price += 50 * it.nb * discount;
         } else if (it.type === 'JACKET') {
-          p += 100 * it.nb * d;
+          price += 100 * it.nb * discount;
         }
         // else if (it.type === "SWEATSHIRT") {
         //   p += 80 * it.nb;
         // }
       }
     } else {
-      if (b.items === null) {
+      if (basket.items === null) {
         return '0';
       }
 
-      for (let i = 0; i < b.items.length; i++) {
-        const it = b.items[i];
+      for (let i = 0; i < basket.items.length; i++) {
+        const it = basket.items[i];
 
         if (it.type === 'TSHIRT') {
-          p += 30 * it.nb * d;
+          price += 30 * it.nb * discount;
         } else if (it.type === 'DRESS') {
-          p += 50 * it.nb * 0.8 * d;
+          price += 50 * it.nb * 0.8 * discount;
         } else if (it.type === 'JACKET') {
-          p += 100 * it.nb * 0.9 * d;
+          price += 100 * it.nb * 0.9 * discount;
         }
         // else if (it.type === "SWEATSHIRT") {
         //   p += 80 * it.nb;
@@ -98,27 +102,27 @@ export class ShoppingController {
     }
 
     try {
-      if (b.type === 'STANDARD_CUSTOMER') {
-        if (p > 200) {
-          throw new Error(`Price (${p}) is too high for standard customer`);
+      if (basket.type === 'STANDARD_CUSTOMER') {
+        if (price > 200) {
+          throw new Error(`Price (${price}) is too high for standard customer`);
         }
-      } else if (b.type === 'PREMIUM_CUSTOMER') {
-        if (p > 800) {
-          throw new Error(`Price (${p}) is too high for premium customer`);
+      } else if (basket.type === 'PREMIUM_CUSTOMER') {
+        if (price > 800) {
+          throw new Error(`Price (${price}) is too high for premium customer`);
         }
-      } else if (b.type === 'PLATINUM_CUSTOMER') {
-        if (p > 2000) {
-          throw new Error(`Price (${p}) is too high for platinum customer`);
+      } else if (basket.type === 'PLATINUM_CUSTOMER') {
+        if (price > 2000) {
+          throw new Error(`Price (${price}) is too high for platinum customer`);
         }
       } else {
-        if (p > 200) {
-          throw new Error(`Price (${p}) is too high for standard customer`);
+        if (price > 200) {
+          throw new Error(`Price (${price}) is too high for standard customer`);
         }
       }
     } catch (e) {
       throw new HttpException((e as Error).message, HttpStatus.BAD_REQUEST);
     }
 
-    return String(p);
+    return String(price);
   }
 }
